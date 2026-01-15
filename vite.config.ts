cfd6ca35e6850a873df73e8dace221a594cdc48b
Antonio Fonseca <antoniocarlos.fernandes@hotmail.com>
2026-01-15 10:47:07 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist"
-  },
   base: "/",
   server: {
     port: 5173
   },
   build: {
     outDir: "dist"
-  }
-    port: 5173
   }
 });
