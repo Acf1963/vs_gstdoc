8391da6e89cecbf91db8c0b54f7e068cd5dfce03
António Fernandes <antoniocarlos.fernandes@hotmail.com>
2026-01-20 10:54:30 +0100

    upload frontend/gestdoc-vite/src/layout/Layout.tsx

--- frontend/gestdoc-vite/src/layout/Layout.tsx
+++ frontend/gestdoc-vite/src/layout/Layout.tsx
@@ -1,18 +1,26 @@
-import Sidebar from './Sidebar'
-import Header from './Header'
-import Breadcrumbs from './Breadcrumbs'
+import { ReactNode } from "react";
+import { NavLink } from "react-router-dom";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex h-screen bg-gray-50 text-gray-900">
-      <Sidebar />
-      <div className="flex flex-col flex-1">
-        <Header />
-        <div className="px-6 py-4">
-          <Breadcrumbs />
-          <main className="mt-4">{children}</main>
-        </div>
-      </div>
+    <div className="min-h-screen flex flex-col md:flex-row bg-gray-100">
+      {/* Sidebar */}
+      <aside className="md:w-64 w-full bg-blue-900 text-white flex md:flex-col flex-row justify-around items-center p-4">
+        <NavLink to="/" className="hover:text-blue-300">
+          <img src="/icons/home.svg" alt="Home" className="w-6 h-6" />
+        </NavLink>
+        <NavLink to="/documentos" className="hover:text-blue-300">
+          <img src="/icons/docs.svg" alt="Documentos" className="w-6 h-6" />
+        </NavLink>
+        <NavLink to="/nova" className="hover:text-blue-300">
+          <img src="/icons/nova.svg" alt="Nova Solicitação" className="w-6 h-6" />
+        </NavLink>
+      </aside>
+
+      {/* Main content */}
+      <main className="flex-1 p-4 overflow-auto">
+        {children}
+      </main>
     </div>
-  )
+  );
 }
