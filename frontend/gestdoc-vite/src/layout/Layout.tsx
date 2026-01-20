588f710b115fd00cd683640bf4c8faa727c34a88
António Fernandes <antoniocarlos.fernandes@hotmail.com>
2026-01-20 12:38:37 +0100

    upload frontend/gestdoc-vite/src/layout/Layout.tsx

--- frontend/gestdoc-vite/src/layout/Layout.tsx
+++ frontend/gestdoc-vite/src/layout/Layout.tsx
@@ -8,15 +8,27 @@ export default function Layout({ children }: { children: ReactNode }) {
       {/* Sidebar */}
       <aside className="md:w-56 w-full bg-blue-900 text-white flex md:flex-col flex-row justify-around items-center py-4 px-2 gap-4 md:gap-6">
         <NavLink to="/" className="hover:text-blue-300">
-          <img src="/icons/home.svg" alt="Home" className="w-5 h-5 md:w-6 md:h-6" />
+          <img
+            src={new URL("/icons/home.svg", import.meta.url).href}
+            alt="Home"
+            className="w-6 h-6 object-contain"
+          />
         </NavLink>
 
         <NavLink to="/documentos" className="hover:text-blue-300">
-          <img src="/icons/docs.svg" alt="Documentos" className="w-5 h-5 md:w-6 md:h-6" />
+          <img
+            src={new URL("/icons/docs.svg", import.meta.url).href}
+            alt="Documentos"
+            className="w-6 h-6 object-contain"
+          />
         </NavLink>
 
         <NavLink to="/nova" className="hover:text-blue-300">
-          <img src="/icons/nova.svg" alt="Nova Solicitação" className="w-5 h-5 md:w-6 md:h-6" />
+          <img
+            src={new URL("/icons/nova.svg", import.meta.url).href}
+            alt="Nova Solicitação"
+            className="w-6 h-6 object-contain"
+          />
         </NavLink>
       </aside>
 
